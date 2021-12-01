e3459078886321e8b69045c2b3294b1d7ccda78b
Flavio Copes <flavio@flaviocopes.com>
2021-12-01 15:41:40 +0100

    Airbnb clone, implement login

--- components/LoginModal.js
+++ components/LoginModal.js
@@ -1,21 +1,49 @@
+import { useState } from 'react'
+import axios from 'axios'
+
 export default function LoginModal(props) {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const submit = async () => {
+    const response = await axios.post('/api/auth/login', {
+      email,
+      password
+    })
+    console.log(response)
+
+    if (response.data.status === 'error') {
+      alert(response.data.message)
+    }
+  }
+
   return (
     <>
       <h2>Log in</h2>
       <div>
         <form
           onSubmit={event => {
-            alert("Sign up!")
+            submit()
             event.preventDefault()
           }}
         >
-          <input id='email' type='email' placeholder='Email address' />
-          <input id='password' type='password' placeholder='Password' />
+          <input
+            id='email'
+            type='email'
+            placeholder='Email address'
+            onChange={event => setEmail(event.target.value)}
+          />
+          <input
+            id='password'
+            type='password'
+            placeholder='Password'
+            onChange={event => setPassword(event.target.value)}
+          />
           <button>Log in</button>
         </form>
       </div>
       <p>
-        Don't have an account yet?{" "}
+        Don't have an account yet?
         <a href='javascript:;' onClick={() => props.showSignup()}>
           Sign up
         </a>
